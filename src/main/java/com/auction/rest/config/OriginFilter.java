bdaf0e30d597594fedd568887bc09f439a59ec3e
Pradeep Murugesan <pradeep_murugesan@trimble.com>
2016-06-18 23:42:55 +0530

    fixing the build break

--- src/main/java/com/auction/rest/config/OriginFilter.java
+++ src/main/java/com/auction/rest/config/OriginFilter.java
@@ -1,5 +1,6 @@
 package com.auction.rest.config;
 
+import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -7,7 +8,6 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class OriginFilter implements Filter {
 
